570c1de1a18809b41f1898a0be14a4f48dcb2c50
Ellen <ellenmerchant@gmail.com>
2018-04-13 20:39:43 +0100

    Downgraded Laravel from 5.6 to 5.5 for PHP 7.0 compatibility

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,5 @@
 
 window._ = require('lodash');
-window.Popper = require('popper.js').default;
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -50,7 +49,7 @@ if (token) {
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: 'your-pusher-key',
+//     cluster: 'mt1',
 //     encrypted: true
 // });
